163742db0221ffc78a597966075c6cd922a3f6b9
markr1395 <130992618+markr1395@users.noreply.github.com>
2023-04-21 14:35:52 -0400

    Update main.m

--- code/main.m
+++ code/main.m
@@ -6,4 +6,17 @@ close all;
 %% Local Path Planning to Satellite
 
 %% Robotic Arm Manuevering
+%% Initialization
+addpath('mr')
+model = load_model();
 
+%%
+N = 20;
+z0 = rand((N+1)*(2*model.nDof+model.nInputs) + 1,1);
+options = optimoptions('fmincon','Display','iter','Algorithm','interior-point',...
+    'MaxIterations',750,'MaxFunctionEvaluations',3000000);
+x_start = rand(3,1);
+z = fmincon(@(z)cost(z,N,model),z0,[],[],[],[],[],[],@(z)nonlcon(z,N,model),options);
+
+%%
+[x,u,T] = extract(z,N,model);
