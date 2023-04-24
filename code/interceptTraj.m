fa56d58b08982065312b94fd9507e6e9580d0d7c
Joey Caley <Caley.11@buckeyemail.osu.edu>
2023-04-24 01:55:50 -0400

    Able to compute a trajectory for a random LEO orbit from a random starting position on an equtorial orbit. Orbits must share the same altitude.

---
+++ code/interceptTraj.m
@@ -0,0 +1,82 @@
+function [dirOfApr, deltaV, tElapsed] = interceptTraj(OE_g, OE_c, k)
+% constants
+mu_Earth = 3.986004*10^14;
+
+% extract values from orbital elements
+a = OE_g(1);
+R = a;
+
+fg = OE_g(end);
+fc = OE_c(end);
+    
+Pog = 2*pi*sqrt(a^3/mu_Earth);
+nog = 2*pi/Pog;
+
+% calculate beta
+beta = fg - fc;
+
+% assume inner orbit with an approach from the bottom
+dirOfApr = 0;
+
+% check if goal is ahead of chaser
+if beta > 0
+    
+    % check if goal spacecraft is ahead of chase spacecraft by more than a
+    % half period
+    if beta >= pi
+        % need to slow down
+        % outer orbit, approach from top
+        dirOfApr = 1;
+        
+        [deltaV, tElapsed] = outerOrbit(k,beta,Pog,nog,R,mu_Earth);
+    else
+        % need to speed up
+        % inner orbit, approach from bottom
+        dirOfApr = 0;
+
+        [deltaV, tElapsed] = innerOrbit(k,beta,Pog,nog,R,mu_Earth);
+    end
+
+% beta is negative, chaser is ahead of goal
+else
+    % eliminate negative sign
+    beta = abs(beta);
+
+    % check if chaser is ahead of goal by more than a half period
+    if beta > pi
+        % need to speed up
+        % inner orbit, approach from bottom
+        dirOfApr = 0;
+
+        [deltaV, tElapsed] = innerOrbit(k,beta,Pog,nog,R,mu_Earth);
+    else
+        % need to slow down
+        % outer orbit, approach from top
+        dirOfApr = 1;
+
+        [deltaV, tElapsed] = outerOrbit(k,beta,Pog,nog,R,mu_Earth);
+    end
+end
+
+end
+
+
+function [deltaV, tElapsed] = outerOrbit(k,beta,Pog,nog,R,mu_Earth)
+
+Pt = Pog + (2*pi - beta)/(k*nog);
+at = (mu_Earth*Pt^2/(4*pi^2))^(1/3);
+
+deltaV = sqrt(2*mu_Earth/R - mu_Earth/at) - sqrt(mu_Earth/R);
+tElapsed = k*Pt;
+
+end
+
+function [deltaV, tElapsed] = innerOrbit(k,beta,Pog,nog,R,mu_Earth)
+
+Pt = Pog - beta/(k*nog);
+at = (mu_Earth*Pt^2/(4*pi^2))^(1/3);
+
+deltaV = sqrt(mu_Earth/R) - sqrt(2*mu_Earth/R - mu_Earth/at);
+tElapsed = k*Pt;
+
+end
--- code/main.m
+++ code/main.m
@@ -1,22 +1,112 @@
 clear all;
 close all;
 
-%% Orbital Rendevouz
+deg2rad = pi/180;
+R_earth = 6378e03;
+minGap = 10*deg2rad;
+mu_Earth = 3.986004*10^14;
+
+%% Orbital Rendezvous
+
+% choose solutions to explore
+k = 1:10;
+
+% define orbit
+a = 300e03 + R_earth;
+e = 0;
+% i = 30*deg2rad;
+i = 2*pi*rand;
+RAAN = 2*pi*rand;
+% RAAN = 190*deg2rad;
+argP = 0;
+
+%-INCLINATION & RAAN CHANGE------------------------------------------------
+f = 2*pi*rand;
+% fc = 200*deg2rad;
+
+[deltaVTotal, tElapsedTotal,thetaTrav] = planeChange(a,i,RAAN,f);
+
+thetaTrav = 0:.05:thetaTrav;
+
+xTrav = a*cos(thetaTrav);
+yTrav = a*sin(thetaTrav);
+zTrav = zeros(1,length(thetaTrav));
+
+%-PHASING MANUEVER---------------------------------------------------------
+
+% g refers to goal, the spacecraft we are trying to reach
+% c refers to chaser, the spacecraft we control
+fg = 2*pi*rand;
+% fc = chaseAnamoly(fg,minGap);
+fc = 0;
+
+% create orbital elements
+OE_g = [a e i RAAN argP fg];
+OE_c = [a e i RAAN argP fc];
+
+% create 
+
+% run all solutions
+for i = 1:length(k)
+    [dirOfApr, deltaV(i), tElapsed(i)] = interceptTraj(OE_g,OE_c,k(i));
+end
+
+% identify impossible solutions and remove them
+i_possible = deltaV == real(deltaV);
+
+k = k(i_possible);
+deltaV = deltaV(i_possible);
+tElapsed = tElapsed(i_possible);
+
+% Create sum vectors
+deltaVTotal = deltaVTotal + 2*deltaV;
+tElapsedTotal = tElapsedTotal + tElapsed;
+
+% output direction of approach
+if dirOfApr == 0
+    fprintf("Chaser spacecraft approaches from bottom with inner transfer orbit\n")
+else
+    fprintf("Chaser spacecraft approaches from top with outer transfer orbit\n")
+end
+
+% plot time to intercept
+figure
+plot(k,tElapsedTotal./3600,'LineWidth',1.5)
+xlabel("k")
+ylabel("Time (hr)")
+title("Total Time to Reach Serviced Spacecraft")
+grid on
+
+% plot delta V required
+figure
+plot(k,deltaVTotal,'LineWidth',1.5)
+xlabel("k")
+ylabel("\Deltav Required (m/s)")
+title("Total \Deltav to Reach Serviced Spacecraft")
+grid on
+
+% figure
+% for i = 1:length(thetaTrav)
+%     scatter3(0,0,0,'SizeData',20)
+%     plot3(xTrav(1:i),yTrav(1:i),zTrav(1:i))
+%     grid on
+%     pause(.05)
+% end
 
 %% Local Path Planning to Satellite
 
 %% Robotic Arm Manuevering
 %% Initialization
-addpath('mr')
-model = load_model();
-
-%%
-N = 20;
-z0 = rand((N+1)*(2*model.nDof+model.nInputs) + 1,1);
-options = optimoptions('fmincon','Display','iter','Algorithm','interior-point',...
-    'MaxIterations',750,'MaxFunctionEvaluations',3000000);
-x_start = rand(3,1);
-z = fmincon(@(z)cost(z,N,model),z0,[],[],[],[],[],[],@(z)nonlcon(z,N,model),options);
-
-%%
-[x,u,T] = extract(z,N,model);
+% addpath('mr')
+% model = load_model();
+% 
+% %%
+% N = 20;
+% z0 = rand((N+1)*(2*model.nDof+model.nInputs) + 1,1);
+% options = optimoptions('fmincon','Display','iter','Algorithm','interior-point',...
+%     'MaxIterations',750,'MaxFunctionEvaluations',3000000);
+% x_start = rand(3,1);
+% z = fmincon(@(z)cost(z,N,model),z0,[],[],[],[],[],[],@(z)nonlcon(z,N,model),options);
+% 
+% %%
+% [x,u,T] = extract(z,N,model);
